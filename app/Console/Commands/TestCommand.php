0c125d9707836c71675ccb2443446b123bf2a470
youyingxiang <1365831278@qq.com>
2021-01-14 16:30:17 +0800

    test serverless schedule

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:console-log';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Console log';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Log::info(
+            Str::replaceArray(
+                "?",
+                [now()->format('Y-m-d H:i:s')],
+                "current time: ?"
+            )
+        );
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\TestCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(TestCommand::class)->everyMinute();
     }
 
     /**

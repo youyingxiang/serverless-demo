5ce475b0bd5c08e786a1b735a085bbbd59cb1f8f
youyingxiang <1365831278@qq.com>
2021-01-14 17:33:25 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(TestCommand::class)->everyMinute();
+        $schedule->command(TestCommand::class)->everyThreeHours();
     }
 
     /**
